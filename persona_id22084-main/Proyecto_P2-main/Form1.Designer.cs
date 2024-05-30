535c08e266654202e129732662d912944cad7cc9
PriscilaID22084 <id22084@dasc.edu.mx>
2024-05-30 01:08:08 -0600

    Add files via upload

---
+++ persona_id22084-main/Proyecto_P2-main/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace persona_herencia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Salir = new System.Windows.Forms.Button();
+            this.limpiar = new System.Windows.Forms.Button();
+            this.guardar = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.validar = new System.Windows.Forms.Button();
+            this.btn_radio_docente = new System.Windows.Forms.RadioButton();
+            this.btn_radio_alumno = new System.Windows.Forms.RadioButton();
+            this.btn_radio_empleado = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Salir);
+            this.groupBox2.Controls.Add(this.limpiar);
+            this.groupBox2.Controls.Add(this.guardar);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 171);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(315, 252);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(211, 180);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(95, 43);
+            this.Salir.TabIndex = 13;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click_1);
+            // 
+            // limpiar
+            // 
+            this.limpiar.Location = new System.Drawing.Point(110, 180);
+            this.limpiar.Name = "limpiar";
+            this.limpiar.Size = new System.Drawing.Size(95, 43);
+            this.limpiar.TabIndex = 12;
+            this.limpiar.Text = "Limpiar";
+            this.limpiar.UseVisualStyleBackColor = true;
+            this.limpiar.Click += new System.EventHandler(this.limpiar_Click_1);
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(9, 180);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(95, 43);
+            this.guardar.TabIndex = 4;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = true;
+            this.guardar.Click += new System.EventHandler(this.guardar_Click_2);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(176, 153);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(176, 127);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(176, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(176, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(176, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SUELDO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CARRERA/PUESTO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DNI / MATRICULA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FECHA DE NACIMIENTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE COMPLETO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EDAD";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.validar);
+            this.groupBox1.Controls.Add(this.btn_radio_docente);
+            this.groupBox1.Controls.Add(this.btn_radio_alumno);
+            this.groupBox1.Controls.Add(this.btn_radio_empleado);
+            this.groupBox1.Location = new System.Drawing.Point(188, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 139);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // validar
+            // 
+            this.validar.Location = new System.Drawing.Point(7, 104);
+            this.validar.Name = "validar";
+            this.validar.Size = new System.Drawing.Size(95, 29);
+            this.validar.TabIndex = 3;
+            this.validar.Text = "VALIDAR";
+            this.validar.UseVisualStyleBackColor = true;
+            this.validar.Click += new System.EventHandler(this.validar_Click);
+            // 
+            // btn_radio_docente
+            // 
+            this.btn_radio_docente.AutoSize = true;
+            this.btn_radio_docente.Location = new System.Drawing.Point(7, 65);
+            this.btn_radio_docente.Name = "btn_radio_docente";
+            this.btn_radio_docente.Size = new System.Drawing.Size(102, 17);
+            this.btn_radio_docente.TabIndex = 2;
+            this.btn_radio_docente.TabStop = true;
+            this.btn_radio_docente.Text = "Soy un Docente";
+            this.btn_radio_docente.UseVisualStyleBackColor = true;
+            // 
+            // btn_radio_alumno
+            // 
+            this.btn_radio_alumno.AutoSize = true;
+            this.btn_radio_alumno.Location = new System.Drawing.Point(7, 42);
+            this.btn_radio_alumno.Name = "btn_radio_alumno";
+            this.btn_radio_alumno.Size = new System.Drawing.Size(95, 17);
+            this.btn_radio_alumno.TabIndex = 1;
+            this.btn_radio_alumno.TabStop = true;
+            this.btn_radio_alumno.Text = "Soy un alumno";
+            this.btn_radio_alumno.UseVisualStyleBackColor = true;
+            // 
+            // btn_radio_empleado
+            // 
+            this.btn_radio_empleado.AutoSize = true;
+            this.btn_radio_empleado.Location = new System.Drawing.Point(7, 19);
+            this.btn_radio_empleado.Name = "btn_radio_empleado";
+            this.btn_radio_empleado.Size = new System.Drawing.Size(107, 17);
+            this.btn_radio_empleado.TabIndex = 0;
+            this.btn_radio_empleado.TabStop = true;
+            this.btn_radio_empleado.Text = "Soy un empleado";
+            this.btn_radio_empleado.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(351, 436);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button limpiar;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button validar;
+        private System.Windows.Forms.RadioButton btn_radio_docente;
+        private System.Windows.Forms.RadioButton btn_radio_alumno;
+        private System.Windows.Forms.RadioButton btn_radio_empleado;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ persona_id22084-main/Proyecto_P2-main/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DATOS GUARDOS");
+        } // Guardar
+        private void validar_Click(object sender, EventArgs e)
+        {
+            // Obtiene la ruta del directorio base del proyecto
+            string directorioBase = AppDomain.CurrentDomain.BaseDirectory;
+            // Imprimimos la ruta, para conocer donde se encuentra nuestro directorio
+            Console.WriteLine("Ruta base: " + directorioBase);
+
+
+            string rutaImagenRelativa = "";
+
+            if (btn_radio_empleado.Checked)
+            {
+                // Cuando seleccionamos el perfil de empleado habilitamos el campo de sueldo
+             
+
+                // Convinamos la dos rutas en donde tenemos nuestro proyecto y donde tenemos guardado nuestras imagenes
+                rutaImagenRelativa = Path.Combine(directorioBase, @"..\..\imagenes\empleado.jpg");
+            }
+            if (btn_radio_alumno.Checked)
+            {
+                // Cuando seleccionamos el perfil de alumno desabilitamos el campo de sueldo
+               
+                rutaImagenRelativa = Path.Combine(directorioBase, @"..\..\imagenes\estudiante.png");
+            }
+            if (btn_radio_docente.Checked)
+            {
+                // Cuando seleccionamos el perfil de empleado habilitamos el campo de sueldo
+               
+
+                rutaImagenRelativa = Path.Combine(directorioBase, @"..\..\imagenes\docente.jpg");
+            }
+
+
+            // Optenemos la ruta completa, desde la ubicacion del disco hasta donde se encuentra nuestra imagen para mostrar la imagen en el picturebox
+            string rutaImagenAbsoluta = Path.GetFullPath(rutaImagenRelativa);
+            Console.WriteLine(rutaImagenAbsoluta);
+
+
+            if (System.IO.File.Exists(rutaImagenAbsoluta))
+            {
+                // Carga la imagen en el PictureBox
+                pictureBox1.Image = Image.FromFile(rutaImagenAbsoluta);
+            }
+
+
+
+
+        } // Validad
+
+        private void guardar_Click_1(object sender, EventArgs e)
+        {
+            if (btn_radio_alumno.Checked)
+            {
+
+                string nom = textBox1.Text;
+                string fech = textBox2.Text;
+                int edad = Convert.ToInt32(textBox3.Text);
+                int mat = Convert.ToInt32(textBox4.Text);
+                string carrera = textBox5.Text;
+                Alumno alumno = new Alumno(nom, edad, fech, carrera, mat);
+            }
+            else
+            {
+                if (btn_radio_empleado.Checked)
+                {
+
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int mat = Convert.ToInt32(textBox4.Text);
+                    string puesto = textBox5.Text;
+                    float sueldo = Convert.ToSingle(textBox6.Text);
+                    Empleado empleado = new Empleado(nom, edad, fech, puesto, sueldo);
+                }
+                else if (btn_radio_docente.Checked)
+                {
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int mat = Convert.ToInt32(textBox4.Text);
+                    string puesto = textBox5.Text;
+                    float sueldo = Convert.ToSingle(textBox6.Text);
+                    Docente docente = new Docente(nom, edad, fech, puesto, sueldo);
+                }
+                else
+                {
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                }
+            }
+        } // Guardar
+
+        private void guardar_Click_2(object sender, EventArgs e)
+        {
+            // Enviamos un messagebox, para mostrar informacion al usuario que sus datos han sido guardados
+            MessageBox.Show("Datos del usuario guardado con exito", "Exito.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void limpiar_Click_1(object sender, EventArgs e)
+        {
+            // se hace de tarea
+            // debe de borrar las cajas de texto
+
+            // Llamamos al nombre de nuestro textbox y llamando a su metodo Clear(), para poder limpiar el textbox
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+
+            // Llamamos al nombre del radio button y colocamos su propiedad Checked en false
+            btn_radio_alumno.Checked = false;
+            btn_radio_docente.Checked = false;
+            btn_radio_empleado.Checked = false;
+        }
+
+        private void Salir_Click_1(object sender, EventArgs e)
+        {
+            // this hace referencia a la ventana  actuala, llamamos su metodo close para cerrar la ventana
+            this.Close();
+        }
+    } // FORM
+
+} // namespace
